2a62962228d866b7b25b893787ee92362709454e
Ashwin-308 <ashwinharia308@gmail.com>
2025-03-25 02:03:49 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import logo from './logo.svg';
-import './App.css';
+import Cell from "./components/Cell"
+import {useState,useEffect} from "react"
 
-function App() {
+const  App= () =>{
+  const [cells,setcells] = useState(["","","","","","","","",""])
+  const [winmsg,setmsg] = useState(null)
+  const [go,setgo] = useState("circle")
+  const msg = "it is now " +go +"'s turn "
+  const checkscore = () => { const wincombos =  [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    wincombos.forEach(array => {
+       let circlewins = array.every(cell =>cells[cell] === "circle")
+       if(circlewins)
+       {
+        setmsg("circle wins")
+        return 
+       }
+       
+      }
+    )
+
+    wincombos.forEach(array => {
+      let crosswins = array.every(cell =>cells[cell] === "cross")
+      if(crosswins)
+      {
+       setmsg("cross wins")
+       return 
+      }
+    }
+  )
+      
+    
+
+    
+  }
+  useEffect(() => { checkscore()})
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "gameboard">
+           {cells.map((cell,index) => <Cell  key = {index} id = {index} cell = {cell}  go = {go} setgo = {setgo} cells = {cells} setcells = {setcells} winmsg = {winmsg}/>)}
+      </div>
+      <p>{winmsg || setmsg}</p>
+      <div>{msg}</div>
     </div>
+    
   );
 }
 
